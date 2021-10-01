7182bfeb57a166abb672fedf6a1416e0bc088318
Pete Hawkins <petey@hey.com>
2021-10-01 20:09:36 +0100

    Install turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,9 @@
 // Entry point for the build script in your package.json
 import Rails from "@rails/ujs";
+import "@hotwired/turbo-rails";
 import * as ActiveStorage from "@rails/activestorage";
 import "./channels";
+import "./controllers";
 
 Rails.start();
 ActiveStorage.start();
